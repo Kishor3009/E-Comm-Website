44e21fe4522b8fda910d8ca8ea8b74b021ea451c
Kishor3009 <shinde.kishor3009@gmail.com>
2024-01-09 12:04:39 +0530

    update

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -103,7 +103,7 @@ const CartPage = () => {
                 <div className="row card flex-row" key={p._id}>
                   <div className="col-md-4">
                     <img
-                      src={`https://e-comm-website-sandy.vercel.app/api/v1/product/product-photo/${p._id}`}
+                      src={`https://ecommerce-backend-wxys.onrender.com/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="100%"
