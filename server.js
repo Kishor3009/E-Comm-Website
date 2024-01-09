a37ba0f8a18a23919b476c71a600d64372105aee
Kishor3009 <shinde.kishor3009@gmail.com>
2024-01-09 16:31:59 +0530

    update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("https://ecommerce-backend-wxys.onrender.com/api/v1/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
